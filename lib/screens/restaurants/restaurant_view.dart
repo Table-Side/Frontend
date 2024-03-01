d7f0c2773d60094d2a29ea6bac399d36d8feb306
Ruth Blackman <rkjb410@gmail.com>
2024-03-01 14:58:49 +0000

    book table form UI

--- lib/screens/restaurants/restaurant_view.dart
+++ lib/screens/restaurants/restaurant_view.dart
@@ -1,10 +1,51 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:table_side/components/CustomAppBar.dart';
 
 // TODO: ALL VALUES HARDCODED ATM NEEDS TO BE CHANGED TO USE VALUES FROM DB
 
-class RestaurantView extends StatelessWidget {
+class RestaurantView extends StatefulWidget {
   const RestaurantView({super.key});
+
+  @override
+  State<RestaurantView> createState() => _RestaurantViewState();
+}
+
+class _RestaurantViewState extends State<RestaurantView> {
+  String numberOfPeople = "1"; // TODO: change type if needed
+  TimeOfDay selectedTime = TimeOfDay.now();
+  DateTime selectedDate = DateTime.now();
+
+  // display time picker
+  Future<void> _selectTime(BuildContext context) async {
+    final TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: selectedTime,
+      initialEntryMode: TimePickerEntryMode.inputOnly,
+    );
+    if (picked != null && picked != selectedTime) {
+      setState(() {
+        selectedTime = picked;
+      });
+    }
+  }
+
+  // display calendar
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+        context: context,
+        initialDate: selectedDate,
+        firstDate: DateTime(2015, 8),
+        lastDate: DateTime(2101),
+      initialEntryMode: DatePickerEntryMode.calendarOnly,
+    );
+    if (picked != null && picked != selectedDate) {
+      setState(() {
+        selectedDate = picked;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,6 +64,108 @@ class RestaurantView extends StatelessWidget {
                     color: const Color(0xff5603AD),
                     onPressed: () {
                       // TODO: Redirect to book a table form/screen
+                      showDialog(
+                          context: context,
+                          builder: (final context) =>
+                              AlertDialog(
+                                title: const Text("Book a table"),
+                                content: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    MaterialButton(
+                                      onPressed: () {
+                                        _selectDate(context);
+                                      },
+                                      child: const Padding(
+                                        padding: EdgeInsets.symmetric(vertical: 10.0),
+                                        child: Text("Select the date", style: TextStyle(fontSize: 20)),
+                                      ),
+                                    ),
+
+
+                                    MaterialButton(
+                                      onPressed: () {
+                                        _selectTime(context);
+                                      },
+                                      child: const Padding(
+                                        padding: EdgeInsets.symmetric(vertical: 10.0),
+                                        child: Text(
+                                          "Select the time",
+                                          style: TextStyle(fontSize: 20),
+                                        ),
+                                      ),
+                                    ),
+
+                                    SizedBox(
+                                        height: 50.0,
+                                        width: 200.0,
+                                        child: DropdownButton(
+                                          value: numberOfPeople,
+                                          icon: const Icon(Icons.arrow_downward),
+                                          iconSize: 24,
+                                          elevation: 16,
+                                          isExpanded: true,
+                                          style: const TextStyle(color: Colors.deepPurple, fontSize: 20.0),
+                                          underline: Container(
+                                            height: 2,
+                                            color: Colors.deepPurpleAccent,
+                                          ),
+                                          onChanged: (String? newValue) {
+                                            setState(() {
+                                              numberOfPeople = newValue ?? "1";
+                                            });
+                                          },
+                                          items: <String>['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+                                              .map<DropdownMenuItem<String>>((String value) {
+                                            return DropdownMenuItem<String>(
+                                              value: value,
+                                              child: Text(value),
+                                            );
+                                          }).toList(),
+                                        )
+                                    ),
+
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Padding(
+                                          padding: const EdgeInsets.symmetric(vertical: 10.0),
+                                          child: MaterialButton(
+                                            height: 60,
+                                            color: const Color(0xff5603AD),
+                                            onPressed: (){
+                                              Navigator.of(context).pop();
+                                            },
+                                            child: const Text("Cancel", style: TextStyle(
+                                                fontSize: 20,
+                                                fontWeight: FontWeight.bold,
+                                                color: Colors.white),
+                                            ),
+                                          ),
+                                        ),
+
+                                        Padding(
+                                          padding: const EdgeInsets.symmetric(vertical: 10.0),
+                                          child: MaterialButton(
+                                            height: 60,
+                                            color: const Color(0xff5603AD),
+                                            onPressed: (){
+                                              // TODO: API call with date, time, number of people, and user name
+                                              // POST request
+                                            },
+                                            child: const Text("Book!", style: TextStyle(
+                                                fontSize: 20,
+                                                fontWeight: FontWeight.bold,
+                                                color: Colors.white),
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              )
+                      );
                     },
                     minWidth: MediaQuery.of(context).size.width * 0.2,
                     child: const Padding(
